1ccc13d09ac1e921f4de171dae559a4b529af9a1
Adwit Mittal <adwitmittal@gmail.com>
2023-02-15 12:12:32 +0530

    added functions for user and land

--- contracts/KrishiBlock.sol
+++ contracts/KrishiBlock.sol
@@ -1,4 +1,4 @@
-//SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: Unlicensed
 pragma solidity ^0.8.17;
 
 contract Land {
@@ -42,7 +42,7 @@ contract Land {
         string designation;
         string city;
     }
-    
+
     uint inspectorsCount;
     uint public userCount;
     uint public landsCount;
@@ -107,4 +107,76 @@ contract Land {
             return false;
         }
     }
+
+
+
+    //-----------------------------------------------User-----------------------------------------------
+
+    function isUserRegistered(address _addr) public view returns(bool)
+    {
+        if(RegisteredUserMapping[_addr]){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    function registerUser(string memory _name, uint _age, string memory _city,string memory _aadharNumber, string memory _panNumber, string memory _document, string memory _email
+    ) public {
+
+        require(!RegisteredUserMapping[msg.sender]);
+
+        RegisteredUserMapping[msg.sender] = true;
+        userCount++;
+        allUsersList[1].push(msg.sender);
+        AllUsers[userCount]=msg.sender;
+        UserMapping[msg.sender] = User(msg.sender, _name, _age, _city,_aadharNumber,_panNumber, _document,_email,false);
+    }
+
+    function verifyUser(address _userId) public{
+        require(isLandInspector(msg.sender));
+        UserMapping[_userId].isUserVerified=true;
+    }
+
+    function isUserVerified(address id) public view returns(bool){
+        return UserMapping[id].isUserVerified;
+    }
+
+
+
+    //-----------------------------------------------Land-----------------------------------------------
+    function addLand(uint _area, string memory _address, uint landPrice,string memory _allLatiLongi, uint _propertyPID,string memory _surveyNum, string memory _document) public {
+        require(isUserVerified(msg.sender));
+        landsCount++;
+        lands[landsCount] = Landreg(landsCount, _area, _address, landPrice,_allLatiLongi,_propertyPID, _surveyNum , _document,false,payable(msg.sender),false);
+        MyLands[msg.sender].push(landsCount);
+        allLandList[1].push(landsCount);
+    }
+
+    function verifyLand(uint _id) public{
+        require(isLandInspector(msg.sender));
+        lands[_id].isLandVerified=true;
+    }
+    
+    function isLandVerified(uint id) public view returns(bool){
+        return lands[id].isLandVerified;
+    }
+
+
+    function transferOwnership(address _from, address _to, uint _id) public returns(bool){
+        require(isLandInspector(msg.sender),"you should be land inspector to call this function");
+        require(isUserVerified(_from));
+        require(isUserVerified(_to));
+        require(isLandVerified(_id));
+        require(lands[_id].ownerAddress == _from);
+
+        lands[_id].ownerAddress = _to;
+        return true;
+    }
 }
+
+//TO DO
+//consensus implementation - tehsildar(2), witness, bank, vakil, lekhpal(landsinspector), buyer, seller, sdm(contract owner)(1)
+//store history of land records
+//etherscan
+//time to complete a trx
