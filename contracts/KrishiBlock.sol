5c54a48313b17432ee196d1e767ed7ac172021a1
XLazer4 <khannarahul446@gmail.com>
2023-02-15 19:15:25 +0530

    added land record mapping

--- contracts/KrishiBlock.sol
+++ contracts/KrishiBlock.sol
@@ -49,6 +49,10 @@ contract Land {
         string tehsil;
     }
 
+    struct landRecord {
+        address[] pastOwners;
+    }
+
     uint public userCount;
     uint public landsCount;
 
@@ -69,16 +73,20 @@ contract Land {
     mapping(uint => address[]) tehsildarList;
     mapping(address => bool)  registeredTehsildarMapping;
 
+    // uint public recordId;
+    // mapping(uint => mapping(uint => address)) public landRecordMapping;
+
+    mapping (uint => landRecord) public landRecordMapping;
+
+    //-----------------------------------------------SDM-----------------------------------------------
+
     function isSDM(address _addr) public view returns(bool){
-        if(_addr==SDM)
-            return true;
-        else
-            return false;
+        if(_addr==SDM) return true;
+        else return false;
     }
 
     function changeSDM(address _addr)public {
         require(msg.sender==SDM,"you are not SDM");
-
         SDM=_addr;
     }
 
@@ -108,15 +116,15 @@ contract Land {
         }
     }
 
-    function isTehsildar(address _id) public view returns (bool) {
-        if(registeredTehsildarMapping[_id]) return true;
+    function isTehsildar(address _addr) public view returns (bool) {
+        if(registeredTehsildarMapping[_addr]) return true;
         else return false;
     }
-    
+
     //-----------------------------------------------Lekhpal-----------------------------------------------
 
     function addLekhpal(address _addr,string memory _name, uint _age, string memory _designation,string memory _city) public returns(bool){
-        require(SDM==msg.sender,"function caller is not SDM");
+        require(isTehsildar(msg.sender) == true,"function caller is not Tehsildar");
         registeredLekhpalMapping[_addr]=true;
         lekhpalList[1].push(_addr);
         lekhpalMapping[_addr] = Lekhpal(_addr,_name, _age, _designation,_city);
@@ -124,8 +132,8 @@ contract Land {
     }
 
     function removeLekhpal(address _addr) public{
-        require(msg.sender==SDM,"You are not SDM");
-        require(registeredLekhpalMapping[_addr],"Land Inspector not found");
+        require(isTehsildar(msg.sender) == true,"function caller is not Tehsildar");
+        require(registeredLekhpalMapping[_addr],"Lekhpal not found");
         registeredLekhpalMapping[_addr]=false;
         uint len=lekhpalList[1].length;
         for(uint i=0;i<len;i++)
@@ -139,23 +147,18 @@ contract Land {
         }
     }
 
-    function isLekhpal(address _id) public view returns (bool) {
-        if(registeredLekhpalMapping[_id]){
-            return true;
-        }else{
-            return false;
-        }
+    function isLekhpal(address _addr) public view returns (bool) {
+        if(registeredLekhpalMapping[_addr]) return true;
+        else return false;
     }
 
     //-----------------------------------------------User-----------------------------------------------
 
     function isUserRegistered(address _addr) public view returns(bool)
     {
-        if(RegisteredUserMapping[_addr]){
-            return true;
-        }else{
-            return false;
-        }
+        if(RegisteredUserMapping[_addr]) return true;
+        else return false;
+
     }
 
     function registerUser(string memory _name, uint _age, string memory _city,string memory _aadharNumber, string memory _panNumber, string memory _document, string memory _email
@@ -192,22 +195,29 @@ contract Land {
 
     function verifyLand(uint _id) public{
         require(isLekhpal(msg.sender));
+        require(lands[_id].area != 0);
         lands[_id].isLandVerified=true;
     }
-    
+
     function isLandVerified(uint id) public view returns(bool){
         return lands[id].isLandVerified;
     }
 
 
     function transferOwnership(address _from, address _to, uint _id) public returns(bool){
-        require(isLekhpal(msg.sender),"you should be land inspector to call this function");
+        require(isTehsildar(msg.sender),"you should be tehsildar to call this function");
         require(isUserVerified(_from));
         require(isUserVerified(_to));
         require(isLandVerified(_id));
         require(lands[_id].ownerAddress == _from);
-
         lands[_id].ownerAddress = _to;
+
+        // recordId++;
+        // landRecordMapping[_id][recordId] = _from;
+        
+        landRecordMapping[_id] = landRecord(pastOwners.push(_from));
+        //need to figure out how to push into the aaray
+
         return true;
     }
 }
@@ -217,9 +227,6 @@ contract Land {
 //store history of land records
 //etherscan
 //time to complete a trx
-
-// user - has access to user stuff
-// tehsildar - transfer ownership, checks if the buyer and seller agree on a price, approve lekhpal
-// lekhpal -  land verification, buyer seller checks
-// witness - will be present with tehsildar and will witness the transaction happening
-// sdm - admin has everything
+//record id continues to add up for different land id as well
+//land is being verified without its existence
+//tehsildar still shows it is there after being removed
