28000d99826d4203d93b6685ac31222a60cc0308
XLazer4 <khannarahul446@gmail.com>
2023-02-18 14:31:28 +0530

    decrypt file

--- frontend/components/Form/UserConfirmModal.tsx
+++ frontend/components/Form/UserConfirmModal.tsx
@@ -1,104 +1,104 @@
 import {
-  useToast,
-  Modal,
-  ModalOverlay,
-  ModalContent,
-  ModalHeader,
-  ModalCloseButton,
-  ModalBody,
-  ModalFooter,
-  Button,
-  Text,
-  Code,
-} from '@chakra-ui/react';
-import { useMutation } from '@tanstack/react-query';
-import { useRouter } from 'next/router';
-import { FC, useEffect, useState } from 'react';
-import useWeb3Store from '../../utils/web3store';
+	useToast,
+	Modal,
+	ModalOverlay,
+	ModalContent,
+	ModalHeader,
+	ModalCloseButton,
+	ModalBody,
+	ModalFooter,
+	Button,
+	Text,
+	Code,
+} from "@chakra-ui/react";
+import { useMutation } from "@tanstack/react-query";
+import { useRouter } from "next/router";
+import { FC, useEffect, useState } from "react";
+import useWeb3Store from "../../utils/web3store";
 
 function sleep(ms: number) {
-  return new Promise((resolve) => setTimeout(resolve, ms));
+	return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
 export const UserConfirmModal: FC<{
-  isOpen: boolean;
-  onClose: () => void;
-  data: any;
+	isOpen: boolean;
+	onClose: () => void;
+	data: any;
 }> = ({ isOpen, onClose, data }) => {
-  const [hash, setHash] = useState<string>();
-  const toast = useToast();
-  const contract = useWeb3Store((state) => state.contract);
-  const router = useRouter();
-  const mutation = useMutation(async () => {
-    const txn = await contract?.registerUser(
-      data.name,
-      data.age,
-      data.city,
-      data.aadhar,
-      data.pan,
-      data.document,
-      data.email
-    );
-    console.log('Registering User....');
-    await txn.wait();
-    setHash(txn.hash);
-  });
-  const confirmHandler = () => {
-    mutation.mutate();
-  };
-  useEffect(() => {
-    if (mutation.isError) {
-      toast({
-        title: 'Error',
-        description: 'An error occured while registering user',
-        status: 'error',
-        duration: 3000,
-        isClosable: true,
-      });
-    }
-    if (mutation.isSuccess) {
-      toast({
-        title: 'User Created',
-        description: `User creation successful ${hash?.slice(0, 10)}`,
-        status: 'success',
-        duration: 7000,
-        isClosable: true,
-      });
-      onClose();
-      router.push('/user');
-    }
-  }, [mutation.isSuccess, hash]);
-  return (
-    <>
-      <Modal isOpen={isOpen} colorScheme="yellow" onClose={onClose} isCentered>
-        <ModalOverlay bg="blackAlpha.300" backdropFilter="blur(10px)" />
-        <ModalContent>
-          <ModalHeader>Confirm Adding User</ModalHeader>
-          <ModalCloseButton />
-          <ModalBody>
-            Create User with the following details
-            <Code p={2}>{JSON.stringify(data, null, 8)}</Code>
-          </ModalBody>
-          <ModalFooter>
-            <Button
-              variant={'outline'}
-              colorScheme="red"
-              mr={3}
-              onClick={onClose}
-            >
-              Go Back
-            </Button>
-            <Button
-              colorScheme={'green'}
-              onClick={confirmHandler}
-              loadingText="Submitting"
-              isLoading={mutation.isLoading}
-            >
-              Continue
-            </Button>
-          </ModalFooter>
-        </ModalContent>
-      </Modal>
-    </>
-  );
+	const [hash, setHash] = useState<string>();
+	const toast = useToast();
+	const contract = useWeb3Store((state) => state.contract);
+	const router = useRouter();
+	const mutation = useMutation(async () => {
+		const txn = await contract?.registerUser(
+			data.name,
+			data.age,
+			data.city,
+			data.hashes.adhar,
+			data.hashes.pan,
+			data.document,
+			data.email,
+		);
+		console.log("Registering User....");
+		await txn.wait();
+		setHash(txn.hash);
+	});
+	const confirmHandler = () => {
+		mutation.mutate();
+	};
+	useEffect(() => {
+		if (mutation.isError) {
+			toast({
+				title: "Error",
+				description: "An error occured while registering user",
+				status: "error",
+				duration: 3000,
+				isClosable: true,
+			});
+		}
+		if (mutation.isSuccess) {
+			toast({
+				title: "User Created",
+				description: `User creation successful ${hash?.slice(0, 10)}`,
+				status: "success",
+				duration: 7000,
+				isClosable: true,
+			});
+			onClose();
+			router.push("/user");
+		}
+	}, [mutation.isSuccess, hash]);
+	return (
+		<>
+			<Modal isOpen={isOpen} colorScheme="yellow" onClose={onClose} isCentered>
+				<ModalOverlay bg="blackAlpha.300" backdropFilter="blur(10px)" />
+				<ModalContent>
+					<ModalHeader>Confirm Adding User</ModalHeader>
+					<ModalCloseButton />
+					<ModalBody>
+						Create User with the following details
+						<Code p={2}>{JSON.stringify(data, null, 8)}</Code>
+					</ModalBody>
+					<ModalFooter>
+						<Button
+							variant={"outline"}
+							colorScheme="red"
+							mr={3}
+							onClick={onClose}
+						>
+							Go Back
+						</Button>
+						<Button
+							colorScheme={"green"}
+							onClick={confirmHandler}
+							loadingText="Submitting"
+							isLoading={mutation.isLoading}
+						>
+							Continue
+						</Button>
+					</ModalFooter>
+				</ModalContent>
+			</Modal>
+		</>
+	);
 };
--- frontend/pages/user/create.tsx
+++ frontend/pages/user/create.tsx
@@ -1,109 +1,144 @@
-import { Box, Button, useDisclosure, VStack } from "@chakra-ui/react";
+import {
+	Box,
+	Button,
+	Heading,
+	Input,
+	useDisclosure,
+	VStack,
+} from "@chakra-ui/react";
 import { NextPage } from "next";
-import { useState } from "react";
+import { ChangeEvent, useRef, useState } from "react";
 import { useForm } from "react-hook-form";
 import { ethers } from "ethers";
 import lighthouse from "@lighthouse-web3/sdk";
 
 import { FormField, UserConfirmModal } from "../../components/Form";
 
+const _hashes = {
+	pan: "",
+	adhar: "",
+};
+
 const Auth: NextPage = () => {
-  const Signature = async () => {
-    const provider = new ethers.providers.Web3Provider(window.ethereum);
-    const signer = provider.getSigner();
-    const address = await signer.getAddress();
-    const messageRequested = (await lighthouse.getAuthMessage(address)).data
-      .message;
-    const signedMessage = await signer.signMessage(messageRequested);
-    return {
-      signedMessage: signedMessage,
-      publicKey: address,
-    };
-  };
+	const [hashes, setHashes] = useState<typeof _hashes>(_hashes);
+	const Signature = async () => {
+		const provider = new ethers.providers.Web3Provider(window.ethereum);
+		const signer = provider.getSigner();
+		const address = await signer.getAddress();
+		const messageRequested = (await lighthouse.getAuthMessage(address)).data
+			.message;
+		const signedMessage = await signer.signMessage(messageRequested);
+		return {
+			signedMessage: signedMessage,
+			publicKey: address,
+		};
+	};
 
-  const progressCallback = (progressData) => {
-    let percentageDone =
-      100 - (progressData?.total / progressData?.uploaded)?.toFixed(2);
-    console.log(percentageDone);
-  };
+	const progressCallback = (progressData: any) => {
+		let percentageDone = (
+			100 -
+			(progressData.total || 0) / (progressData.uploaded || 0)
+		).toFixed(2);
+		console.log(percentageDone);
+	};
 
-  const deployEncrypted = async (e) => {
-    const sig = await Signature();
-    const apiKey = process.env.NEXT_PUBLIC_API_KEY as string;
-    const response = await lighthouse.uploadEncrypted(
-      e,
-      sig.publicKey,
-      apiKey,
-      sig.signedMessage,
-      progressCallback
-    );
-    console.log(response.data.Hash);
+	const deployEncrypted = async (
+		e: ChangeEvent<HTMLInputElement> | any,
+		name: "adhar" | "pan",
+	) => {
+		const sig = await Signature();
+		const apiKey = process.env.NEXT_PUBLIC_API_KEY as string;
+		const response = await lighthouse.uploadEncrypted(
+			e,
+			sig.publicKey,
+			apiKey,
+			sig.signedMessage,
+			progressCallback,
+		);
+		const cid = response.data.Hash;
+		const { publicKey, signedMessage } = await Signature();
+		const publicKeyUserB = ["0xc5074464eBF403a44D4CFECe2C532aF0D9b6DF22"];
+		const res = await lighthouse.shareFile(
+			publicKey,
+			publicKeyUserB,
+			cid,
+			signedMessage,
+		);
+		console.log("res", res);
+		switch (name) {
+			case "adhar":
+				setHashes({ ...hashes, adhar: res.data.cid });
+				break;
+			case "pan":
+				setHashes({ ...hashes, pan: res.data.cid });
+				break;
+		}
+	};
 
-    const cid = response.data.Hash;
-    const { publicKey, signedMessage } = await Signature();
-    const publicKeyUserB = ["0xc5074464eBF403a44D4CFECe2C532aF0D9b6DF22"];
-    const res = await lighthouse.shareFile(
-      publicKey,
-      publicKeyUserB,
-      cid,
-      signedMessage
-    );
-    console.log(res);
-  };
+	const {
+		isOpen: confirmIsOpen,
+		onOpen: confirmOnOpen,
+		onClose: confirmOnClose,
+	} = useDisclosure();
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+	} = useForm();
+	const [data, setData] = useState<any>();
+	return (
+		<Box p={4}>
+			<VStack spacing={4} maxW="container.md">
+				<FormField name="name" errorsObj={errors.name} register={register} />
+				<FormField name="age" errorsObj={errors.age} register={register} />
+				<FormField name="city" errorsObj={errors.city} register={register} />
+				<Heading textAlign={"left"} w="full" fontSize={"lg"}>
+					Aadhar
+				</Heading>
+				<input
+					required
+					onChange={(e) => deployEncrypted(e, "adhar")}
+					type="file"
+				/>
+				<Heading textAlign={"left"} w="full" fontSize={"lg"}>
+					PAN
+				</Heading>
+				<input
+					required
+					onChange={(e) => deployEncrypted(e, "pan")}
+					type="file"
+				/>
+				<FormField
+					name="document"
+					errorsObj={errors.name}
+					register={register}
+				/>
+				<FormField
+					name="email"
+					inputProps={{ type: "email" }}
+					errorsObj={errors.name}
+					register={register}
+				/>
 
-  const {
-    isOpen: confirmIsOpen,
-    onOpen: confirmOnOpen,
-    onClose: confirmOnClose,
-  } = useDisclosure();
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm();
-  const [data, setData] = useState<any>();
-  return (
-    <Box p={4}>
-      <VStack spacing={4} maxW="container.md">
-        <FormField name="name" errorsObj={errors.name} register={register} />
-        <FormField name="age" errorsObj={errors.age} register={register} />
-        <FormField name="city" errorsObj={errors.city} register={register} />
-        <h3>Aadhar</h3>
-        <input onChange={(e) => deployEncrypted(e)} type="file" />
-        <h3>PAN</h3>
-        <input onChange={(e) => deployEncrypted(e)} type="file" />
-        {/* <FormField name="aadhar" errorsObj={errors.aadhar} register={register} /> */}
-        {/* <FormField name="pan" errorsObj={errors.pan} register={register} /> */}
-        <FormField
-          name="document"
-          errorsObj={errors.name}
-          register={register}
-        />
-        <FormField
-          name="email"
-          inputProps={{ type: "email" }}
-          errorsObj={errors.name}
-          register={register}
-        />
-
-        <Button
-          onClick={handleSubmit((formData) => {
-            setData(formData);
-            confirmOnOpen();
-          })}
-          colorScheme={"yellow"}
-          minW="48"
-        >
-          Add
-        </Button>
-      </VStack>
-      <UserConfirmModal
-        isOpen={confirmIsOpen}
-        onClose={confirmOnClose}
-        data={data}
-      />
-    </Box>
-  );
+				<Button
+					onClick={handleSubmit((formData) => {
+						setData(formData);
+						confirmOnOpen();
+					})}
+					colorScheme={"yellow"}
+					disabled={hashes.adhar === "" || hashes.pan === ""}
+					minW="48"
+				>
+					Add
+				</Button>
+			</VStack>
+			<UserConfirmModal
+				isOpen={confirmIsOpen}
+				onClose={confirmOnClose}
+				data={{ ...data, hashes }}
+			/>
+		</Box>
+	);
 };
 
 export default Auth;
