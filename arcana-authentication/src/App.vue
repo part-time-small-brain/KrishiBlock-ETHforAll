a5ce3bc5dcd174b4cace5ab22b5c81e46e532334
Adwit Mittal <adwitmittal@gmail.com>
2023-02-19 11:41:47 +0530

    added support for arcana

---
+++ arcana-authentication/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+  </div>
+</template>
+
+<script>
+import { getAuthInstance } from '@/lib/auth'
+
+export default {
+  name: 'App',
+  async mounted () {
+    // eslint-disable-next-line no-unused-vars
+    const a = await getAuthInstance()
+    // Provider available at a.provider
+    window.open('//krishiblock-ethforall.vercel.app', '_self')
+  }
+}
+</script>
